ffbc9a1e818621d09b26c43baf87a5a86d72ebce
Amandine Rius <amandine.rius@epitech.eu>
2024-09-05 18:06:47 -0500

    add all the files for the first project

---
+++ ApplePicker/Assets/AppleTree.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    [Header("Inscribed")]
+
+    public GameObject   applePrefab;
+
+    public float        speed = 1f;
+
+    public float        leftAndRightEdge = 24f;
+ 
+    public float        changeDirChance = 0.1f;
+ 
+    public float        appelDropDelay = 1f;
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
