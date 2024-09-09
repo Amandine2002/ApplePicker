13dd2432e46ba7995a2e24246343654cbebdcb6b
Amandine Rius <amandine.rius@epitech.eu>
2024-09-09 00:13:01 -0500

    Finish the project, i have to think about add : Game Over and Start screen and probably other features

--- ApplePicker/Assets/AppleTree.cs
+++ ApplePicker/Assets/AppleTree.cs
@@ -7,7 +7,7 @@ public class AppleTree : MonoBehaviour {
 
     public GameObject   applePrefab;
 
-    public float        speed = 1f;
+    public float        speed = 10f;
 
     public float        leftAndRightEdge = 24f;
  
@@ -16,11 +16,34 @@ public class AppleTree : MonoBehaviour {
     public float        appelDropDelay = 1f;
 
     void Start() {
-        
+        // Start dropping apples
+        Invoke( "DropApple", 2f);
+    }
+
+    void DropApple() {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appelDropDelay);
     }
 
     // Update is called once per frame
     void Update() {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed);   // Move right
+        } else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+    }
+
+
+    void FixedUpdate() {
+        // Random direction changes are now time-based due to FixedUpdate()
+        if (Random.value < changeDirChance) {
+            speed *= -1; // Change direction
+        }
     }
 }
